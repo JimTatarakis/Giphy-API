cc6b39ac63c15ef9b077c07183abbae3aa048bc2
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-20 09:35:57 -0700

    finished product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,47 @@
-// write a var for the queryurl
-// populate them buttons
-// write on click function for buttons
-// on button click generate 10 gifs
-// write a search fxn for the search bar
+let searchTerm = "";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=10&api_key=69Mht6ewKKZaEMo38HgDTlciavTxPRc8"
+var btnArr = ["corgie", "pupper", "doge", "puppy", "cool dog"]
+var btnSec = document.getElementById("button-section");
+var gifArea = document.getElementById("gif-area");
+
+const getGifs = () => {
+    console.log(queryURL);// NOTE: take a look at the url here. it's missing a value for q so it's not searching for anything.
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response);
+            console.log(response.json());// NOTE: you can only read the JSON data once. look at the second answer for https://stackoverflow.com/questions/53511974/javascript-fetch-failed-to-execute-json-on-response-body-stream-is-locked
+            return response.json();
+            // var data = response.json()
+            // .then(function(data){
+            //     console.log(data);
+
+            // })
+            
+            // for (var i = 0; i < data.length; i++) {
+            //     console.log(data[i].embed_url);
+            //  }
+        }).then(function(data){
+            console.log(data);
+        })
+};
+
+for (i = 0; i < btnArr.length; i++) {
+    var btn = document.createElement("A");
+    btn.innerHTML = btnArr[i];
+    btn.setAttribute('class', "waves-effect waves-light btn");
+    btn.setAttribute('id', 'button' + [i]);
+    btnSec.appendChild(btn);
+}
+
+btnSec.onclick = function (event) {
+    let target = event.target;
+    let searchTerm = target.text
+    getGifs();
+    console.log(searchTerm);
+}
+
+
+
+
+// TODO: on button click generate 10 gifs
+// TODO: write a search fxn for the search bar
---
+++ assets/javascript/appPlus.js
@@ -0,0 +1,74 @@
+var btnArr = ["corgie", "pupper", "doge", "puppy", "cool dog"]
+var btnSec = document.getElementById("button-section");
+var gifArea = document.getElementById("gif-area");
+// add event listener for when enter is pressed
+
+document.querySelector('#search').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+        var btn = document.createElement("A");
+        btn.innerHTML = document.getElementById("search").value; //need to get typed data!!!!
+        btn.setAttribute('class', "waves-effect waves-light btn");
+        btnSec.appendChild(btn);
+    }
+});
+
+// call api
+const getGifs = (searchTerm) => {
+    gifArea.innerHTML= "";
+    console.log(searchTerm);
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=10&api_key=69Mht6ewKKZaEMo38HgDTlciavTxPRc8"
+    console.log(queryURL);
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        }).then(function(json){
+            console.log(json);
+            // console.log(JSON.stringify(data));
+            console.log(json.data);
+            for (var i = 0; i < json.data.length; i++) {
+                console.log(json.data[i].embed_url);
+                var gifDiv = document.createElement("DIV");
+                var img = document.createElement("IMG");
+                img.src = json.data[i].images.fixed_height_small_still.url;
+                img.setAttribute('animate', json.data[i].images.original.url);
+                img.setAttribute('still', json.data[i].images.fixed_height_small_still.url);
+                var rating = document.createElement("P");
+                rating.innerHTML = 'Rating:' + json.data[i].rating;
+                gifDiv.appendChild(img);
+                gifDiv.appendChild(rating);
+                gifArea.appendChild(gifDiv);
+                
+             }
+        })
+};
+
+for (i = 0; i < btnArr.length; i++) {
+    var btn = document.createElement("A");
+    btn.innerHTML = btnArr[i];
+    btn.setAttribute('class', "waves-effect waves-light btn");
+    btn.setAttribute('id', 'button' + [i]);
+    btnSec.appendChild(btn);
+}
+// populate the gifs
+btnSec.onclick = function (event) {
+    let target = event.target;
+    let searchTerm = target.text
+
+    getGifs(searchTerm);
+}
+const animateGif = (selectedGif) => {
+    selectedGif.src = selectedGif.getAttribute('animate');
+}
+// animate the gifs
+gifArea.onclick = function (event) {
+    let selectedGif = event.target;
+    // if not animated
+    if( selectedGif.getAttribute('animate') === selectedGif.src){
+        selectedGif.src = selectedGif.getAttribute('still');
+    }
+    else{
+        animateGif(selectedGif);
+    }
+}
+// TODO: make buttons from search bar
